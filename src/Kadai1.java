d16557c4f813bef7c1cd7b9336968462d132a86d
302A-01\cre <a>
2025-06-17 16:07:37 +0900

    Kadai1

--- src/Kadai1.java
+++ src/Kadai1.java
@@ -1,13 +1,16 @@
 public class Kadai1 {
     public static void main(String[] args) {
-        int si = Integer.parseInt(args[0]);
-        for(int i = 1; i <= 1000; i++)
-            if( (i % si) == 0 ){
-                System.out.println(i);
+        try {
+            int si = Integer.parseInt(args[0]);
+            for (int i = 1; i <= 1000; i++)
+                if ((i % si) == 0) {
+                    System.out.println(i);
+                } else {
+                    System.out.print(i + " ");
+                }
+        }catch (NumberFormatException e) {
+            System.out.println("era-");
             }
-            else{
-                System.out.print(i+" ");
-            }
-        }
     }
+}
 
